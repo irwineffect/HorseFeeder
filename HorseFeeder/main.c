ba0cac5ee689d4a176059294a6e8d259d04d3ba3
James Irwin <irwineffect@gmail.com>
2014-12-29 13:07:23 -0800

    initial commit

---
+++ HorseFeeder/main.c
@@ -0,0 +1,17 @@
+/*
+ * main.c
+ *
+ * Created: 12/29/2014 12:54:49 PM
+ *  Author: james
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
